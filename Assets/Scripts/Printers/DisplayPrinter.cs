7660fdb5f0f25540b0a3ffb3c539711c03a0ba2d
chris8736 <christophertam@brandeis.edu>
2021-07-18 10:25:36 -0400

    hand functions with cithria

--- Assets/Scripts/Printers/DisplayPrinter.cs
+++ Assets/Scripts/Printers/DisplayPrinter.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class DisplayPrinter : Printer
 {
     public List<Sprite> cardImages;
+    public Dictionary<string, string> imageDictionary = new Dictionary<string, string>();
+    public GameObject boardDisplay;
+    public Sprite nullImage;
+
+    public void FillImageDictionary()
+    {
+        imageDictionary.Add("Cithria of Cloudfield", "01DE039");
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -14,9 +23,10 @@ public class DisplayPrinter : Printer
         FindTextReferences();
 
         InitializeDictionary();
+        FillImageDictionary();
 
-        playerADeck = LoadDeckFromJson("Assets/Decks/spelltest3.json");
-        playerBDeck = LoadDeckFromJson("Assets/Decks/spelltest3.json");
+        playerADeck = LoadDeckFromJson("Assets/Decks/cithria.json");
+        playerBDeck = LoadDeckFromJson("Assets/Decks/cithria.json");
 
         ResetGame(true);
     }
@@ -55,5 +65,71 @@ public class DisplayPrinter : Printer
         {
             plotter.createDataPoint(playedGamesInMatch - 500, score, finishedMatches * 10);
         }
+    }
+
+    void UpdateText()
+    {
+        base.UpdateText();
+        UpdateDisplayBoard();
+    }
+
+    void UpdateDisplayBoard()
+    {
+        UpdatePlayerOneHand();
+        UpdatePlayerOneBench();
+        UpdatePlayerOneBattlefield();
+        UpdatePlayerTwoHand();
+        UpdatePlayerTwoBench();
+        UpdatePlayerTwoBattlefield();
+    }
+
+    void UpdatePlayerOneHand()
+    {
+        int cardIndex = 1;
+        foreach (Card card in board.playerOneSide.hand.cards)
+        {
+            Sprite cardSprite = null;
+            foreach (Sprite sprite in cardImages)
+            {
+                if (sprite.name.Equals(imageDictionary[card.name]))
+                {
+                    Debug.Log("Image found.");
+                    cardSprite = sprite;
+                }
+            }
+            boardDisplay.transform.Find("Canvas").Find("Player One Hand").Find("Card " + cardIndex).gameObject.GetComponent<Image>().sprite = cardSprite;
+            cardIndex += 1;
+        }
+
+        while (cardIndex <= 10)
+        {
+            boardDisplay.transform.Find("Canvas").Find("Player One Hand").Find("Card " + cardIndex).gameObject.GetComponent<Image>().sprite = nullImage;
+            cardIndex += 1;
+        }
+    }
+
+    void UpdatePlayerOneBench()
+    {
+
+    }
+
+    void UpdatePlayerOneBattlefield()
+    {
+
+    }
+
+    void UpdatePlayerTwoHand()
+    {
+
+    }
+
+    void UpdatePlayerTwoBench()
+    {
+
+    }
+
+    void UpdatePlayerTwoBattlefield()
+    {
+
     }
 }
